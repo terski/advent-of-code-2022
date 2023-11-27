bf78d87695ef50ff66de3e3055b93679ee87b946
Matt Terski <matt.terski@gmail.com>
2023-11-27 16:24:33 +0000

    feat: Add moves and calculate locations
    Added moves, delta object, rope array, and calculate locations for given input

--- src/day09/index.ts
+++ src/day09/index.ts
@@ -16,8 +16,8 @@ const part1 = (rawInput: string) => {
 
     for (let move of moves) {
         const [direction, distance] = move.split(' ');
+        const diff = delta[direction];
         for (let i = 0; i < +distance; i++) {
-            const diff = delta[direction];
             const previous = { row: head.row, column: head.column };
 
             head.row += diff[0];
@@ -37,10 +37,52 @@ const part1 = (rawInput: string) => {
     return locations.size.toString();
 };
 
-const part2 = (rawInput: string) => {
-    const input = parseInput(rawInput);
+type Position = { row: number; column: number };
 
-    return;
+const part2 = (rawInput: string) => {
+    const moves = parseInput(rawInput);
+    const delta: { [key: string]: [number, number] } = {
+        R: [0, 1],
+        L: [0, -1],
+        U: [-1, 0],
+        D: [1, 0],
+    };
+    const rope = Array<Position>(10).fill({ row: 0, column: 0 }, 0, 10);
+    console.log(rope);
+    const locations = new Set([`${rope[9].row},${rope[9].column}`]);
+
+    for (let move of moves) {
+        const [direction, distance] = move.split(' ');
+        const diff = delta[direction];
+        console.log(`Move`);
+        for (let i = 0; i < +distance; i++) {
+            for (let j = 0; j < rope.length; j++) {
+                if (j === 0) {
+                    rope[j].row += diff[0];
+                    rope[j].column += diff[1];
+                } else {
+                    const vert = rope[j].row - rope[j - 1].row;
+                    if (Math.abs(vert) > 1) {
+                        rope[j].row += rope[j].row > rope[j - 1].row ? -1 : 1;
+                        console.log(
+                            `Moving rope[${j}]to ${rope[j].row}, ${rope[j].column}`,
+                        );
+                    }
+                    const horiz = rope[j].column - rope[j - 1].column;
+                    if (Math.abs(horiz) > 1) {
+                        rope[j].column +=
+                            rope[j].column > rope[j - 1].column ? -1 : 1;
+                            console.log(
+                                `Moving rope[${j}] to ${rope[j].row}, ${rope[j].column}`,
+                            );
+                    }
+                }
+            }
+            locations.add(`${rope[-1].row},${rope[-1].column}`);
+        }
+    }
+
+    return locations.size.toString();
 };
 
 run({
@@ -63,13 +105,21 @@ run({
     },
     part2: {
         tests: [
-            // {
-            //   input: ``,
-            //   expected: "",
-            // },
+            {
+                input: `
+                R 5
+                U 8
+                L 8
+                D 3
+                R 17
+                D 10
+                L 25
+                U 20`,
+                expected: '36',
+            },
         ],
         solution: part2,
     },
     trimTestInputs: true,
-    onlyTests: false,
+    onlyTests: true,
 });
